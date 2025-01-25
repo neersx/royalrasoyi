a3831b6b71904c6bfa21a4f0fa127f33203264b9
Neeraj Singh <singh.neer@ymail.com>
2025-01-25 08:29:09 +0530

    About Us replaced with Why Choose Us?

--- royal-rasoyi/src/app/pages/about/about.component.ts
+++ royal-rasoyi/src/app/pages/about/about.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MetaService } from '../../core/services/meta-tags.service';
 
 @Component({
   selector: 'app-about',
@@ -10,5 +11,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  
+  constructor( private readonly metaService: MetaService) {
+    const metaDetails = {
+      title: 'Why choose our Royal रसोई ?  | The Taste you deserve',
+      description: 'Experience the Royal रसोई difference today. Let’s make healthy eating a delicious journey!',
+      image: 'https://ucarecdn.com/e7306f47-376f-4c0b-9ea8-d6af7ee80e68/whyus.jpg',
+      url: 'https://royalrasoyi.com/why-us',
+      type: 'website'
+    };
+  
+     this.metaService.updateMetaTags('about-us', metaDetails);
+  }
 }
