9c13ebb86ad8a1545442295b27e2a8a86375138e
Neeraj Singh <singh.neer@ymail.com>
2025-01-29 08:27:00 +0530

    Prerender details page

--- royal-rasoyi/src/app/app.routes.server.ts
+++ royal-rasoyi/src/app/app.routes.server.ts
@@ -1,10 +1,17 @@
 import { inject } from '@angular/core';
 import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
+import { FoodMenuService } from './services/food-menu.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'why-us',
-    renderMode: RenderMode.Prerender
+    path: 'food-details/:name',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const dataService = inject(FoodMenuService);
+      const ids = await dataService.getFoodMenu().map((x: any) => x.name);
+      return ids.map((name: string) => ({ name }));
+    },
   },
   {
     path: '**',
